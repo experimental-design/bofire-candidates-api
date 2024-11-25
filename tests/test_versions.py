d0f3b67268d87ee2818dbb4ec5923670b8cf75f7
Johannes P. Dürholt <johannespeter.duerholt@evonik.com>
2024-11-25 15:21:12 +0100

    add tests for worker

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -3,7 +3,9 @@ import json
 from tests.conftest import Client
 
 
-def test_candidates_request_validation(client: Client):
+def test_versions(client: Client):
     response = client.get(path="/versions")
     assert response.status_code == 200
-    assert list(json.loads(response.content).keys()) == ["bofire"]
+    assert sorted(json.loads(response.content).keys()) == sorted(
+        ["bofire", "bofire_candidates_api"]
+    )
